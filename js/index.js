0f66e04a84eff6055046ab84ac9461cc55e09883
hurungheea <hurunghee.alexis@gmail.com>
2018-11-05 08:43:15 +0100

    last

--- js/index.js
+++ js/index.js
@@ -1,16 +1,25 @@
 "use strict";
 
+import { stateModel } from "./stateModel.js";
+import { Port } from "./port.js";
+
 window.onload = function()
 {
     if(scion === undefined)
     {
         swal({
-            title:"Error de chargement !!",
+            title:"Erreur de chargement !!",
             type:'error',
             text:'Un problème est survenue lors du téléchargement de la page, veuillez la recharger pour tenter de corrier l\'erreur',
-            footer:'<a onclick="document.location.reload(true);">Recharger la page</a>'
+            footer:'<a style="cursor:pointer;" onclick="document.location.reload(true);">Recharger la page</a>'
         });
     }
-    
-    
+    let interpreter = new scion.Statechart(stateModel);
+    let port = new Port();
+
+    $('.start').click(function()
+    { 
+        interpreter.start();
+        
+    });
 };
---
+++ js/port.js
@@ -0,0 +1,33 @@
+export class Port 
+{   // Constructeur de la class Port
+    constructor()
+    {
+        this.open = false;
+        this.port = 8080;
+    }
+    // Retourne si le port est ouvert ou non
+    isOpen(p_open) 
+    {
+        return this.open;
+    }
+    //Retroune quelle port est écouté
+    isListening(p_port)
+    {
+        return this.port;
+    }
+    // Ouvre ou ferme un port
+    setOpen(p_open)
+    {
+        this.open = p_open;
+    }
+    // Permet de donner un port d'écoute
+    setPort(p_port)
+    {
+        this.port = p_port;
+    }
+    // Affiche dans la console le Port qui est écouté
+    listenTo()
+    {
+        console.log("port listen to : " + this.isListening());
+    }
+}
