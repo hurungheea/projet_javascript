294e3a372c98e6df7afef30516639d600dcca719
hurungheea <ahurunchee@scinfe180.univ-pau.fr>
2018-11-05 13:49:10 +0100

    reccup console

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 import { stateModel } from "./stateModel.js";
 import { Port } from "./port.js";
 
@@ -10,16 +8,19 @@ window.onload = function()
         swal({
             title:"Erreur de chargement !!",
             type:'error',
-            text:'Un problème est survenue lors du téléchargement de la page, veuillez la recharger pour tenter de corrier l\'erreur',
+            text:'Un problème est survenu lors du téléchargement de la page, veuillez la recharger pour tenter de corriger l\'erreur',
             footer:'<a style="cursor:pointer;" onclick="document.location.reload(true);">Recharger la page</a>'
         });
     }
     let interpreter = new scion.Statechart(stateModel);
     let port = new Port();
 
+    let consoleArea = $('.myConsole');
+    consoleArea.attr('readOnly','true');
+    console.log(consoleArea);
+
     $('.start').click(function()
     { 
-        interpreter.start();
-        
+        interpreter.start();      
     });
 };
